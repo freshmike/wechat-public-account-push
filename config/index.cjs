fd14928182b8d71d9abc4e6de7f16b684ebedcdb
来不及开大 <1259649983@qq.com>
2023-07-25 17:47:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'opk196E4mciDvOabLyiQNKV2l1fM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '5jTi8Kp9vWPZP6gYSYApFHanHpAVUdvN3wJCAAMESOg',
+      useTemplateId: 'T4xaeD_3lux5vaIwyWVLGlEIlbVcFVwP2RiRlhgO_0U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-26',
       festivals: [
@@ -48,7 +48,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '5jTi8Kp9vWPZP6gYSYApFHanHpAVUdvN3wJCAAMESOg',
+  CALLBACK_TEMPLATE_ID: 'T4xaeD_3lux5vaIwyWVLGlEIlbVcFVwP2RiRlhgO_0U',
 
   CALLBACK_USERS: [
     {
