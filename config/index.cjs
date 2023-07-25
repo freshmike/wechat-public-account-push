7ff0ed473bdbe93c210c398474a11463baae129f
来不及开大 <1259649983@qq.com>
2023-07-25 17:56:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小可爱',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'opk196E4mciDvOabLyiQNKV2l1fM',
+      id: 'opk196H20gG090xYNp4EBRIFIieg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'T4xaeD_3lux5vaIwyWVLGlEIlbVcFVwP2RiRlhgO_0U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
